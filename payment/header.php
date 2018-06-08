6dc7ca1e9bd796ec3af52e197c6c33a069552063
isabeltiongsk <36809914+isabeltiongsk@users.noreply.github.com>
2018-06-08 09:45:44 +0800

    Add files via upload

---
+++ payment/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+</nav>
+
+       <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    
+    <ul class="nav navbar-nav">
+      <li><a href="../index.php">Schedule</a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Management
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="../staff/staff.php">Staff overview</a></li>
+          <li><a href="../staff/create.php">Add staff</a></li>
+          <li><a href="../product/product.php">Product overview</a></li>
+          <li><a href="../product/create.php">Add product</a></li>
+        </ul>
+      </li>
+      <li  class="active"><a href="payment.php">Payment</a></li>
+      <li><a href="../report/report.php">Report</a></li>
+    </ul>
+  </div>
+</nav> 
