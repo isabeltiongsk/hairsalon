cd6512d07bbcb061f35bcccacc1d230ce62f56ed
isabeltiongsk <36809914+isabeltiongsk@users.noreply.github.com>
2018-06-08 09:44:12 +0800

    Update header.php

--- product/header.php
+++ product/header.php
@@ -14,22 +14,22 @@
        <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">G2 by Govin</a>
+
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="../index.php">Home</a></li>
-      <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Management
+      <li><a href="../index.php">Schedule</a></li>
+      <li class="active" class="dropdown" >
+        <a class="dropdown-toggle"  data-toggle="dropdown" href="#">Management
         <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="../staff.php">Staff overview</a></li>
-          <li><a href="staff/create.php">Add staff</a></li>
-          <li><a href="../product.php">Product overview</a></li>
+        <ul class="dropdown-menu" >
+          <li><a href="../staff/staff.php">Staff overview</a></li>
+          <li><a href="../staff/create.php">Add staff</a></li>
+          <li><a href="product.php">Product overview</a></li>
           <li><a href="create.php">Add product</a></li>
         </ul>
       </li>
-      <li><a href="../payment">Payment</a></li>
-      <li><a href="#">Report</a></li>
+      <li><a href="../payment/payment.php">Payment</a></li>
+      <li><a href="../report/report.php">Report</a></li>
     </ul>
   </div>
 </nav> 
